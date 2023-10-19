2c5ddc764351ceff28c04a4f222347746e08880b
DessyDAM2 <200076@iessanalberto.com>
2023-10-19 10:10:18 +0200

    Second cumitt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.iessanalberto.yopreguntoturespondes"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.iessanalberto.yopreguntoturespondes"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
