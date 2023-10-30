efacd0b439c9ca20b61fb42ac7a32620511211e0
DessyDAM2 <200076@iessanalberto.com>
2023-10-30 11:37:35 +0100

    Second cumitt

--- app/src/main/java/com/iessanalberto/yopreguntoturespondes/MainActivity.kt
+++ app/src/main/java/com/iessanalberto/yopreguntoturespondes/MainActivity.kt
@@ -1,41 +1,44 @@
 package com.iessanalberto.yopreguntoturespondes
 
-import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.iessanalberto.yopreguntoturespondes.ui.theme.YoPreguntoTuRespondesTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             YoPreguntoTuRespondesTheme {
-                val context = LocalContext.current
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    PantallaJuego(context = context)
+                    PantallaJuego()
                 }
             }
         }
@@ -44,24 +47,44 @@ class MainActivity : ComponentActivity() {
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
+    fun PantallaJuego() {
 
-    fun PantallaJuego(context: Context) {
+        val listaPreguntas = mutableListOf(
+            "¿Cuál es el videojuego más vendido de la historia?",
+            "¿Cuál es el mamífero más pesado del mundo?",
+            "¿Cuál es el elemento químico con mayor densidad?",
+            "¿Cuál es la velocidad del sonido en m/s?",
+            "¿Cuál es la capacidad siguiente al Terabyte?"
+        )
 
-        var puntuacion by rememberSaveable { mutableStateOf(0) }
+        val listaRespuestas = mutableListOf(
+            "Tetris",
+            "Ballena Azul",
+            "Osmio",
+            "343",
+            "Petabyte"
+        )
+
+        val context = LocalContext.current
+
+
+        var maxPreguntas by rememberSaveable {
+            mutableStateOf(0)
+        }
+
+        var puntuacion by rememberSaveable {
+            mutableStateOf(0)
+        }
 
         var pregunta by rememberSaveable {
-            mutableStateOf(
-                hashMapOf(
-                    "¿Cuál es el juego más vendido de la historia?" to "Tetris",
-                    "¿Cuál es el mamífero más pesado del mundo?" to "Ballena Azul",
-                    "¿Cuál es el elemento químico con mayor densidad?" to "Osmio",
-                    "¿Cuál es la velocidad del sonido en m/s?" to "344",
-                    "¿Cuál es la capacidad siguiente al Terabyte?" to "Petabyte",
-                ).keys.random()
-            )
+            mutableStateOf(listaPreguntas.random())
         }
 
         var respuesta by rememberSaveable {
+            mutableStateOf(listaRespuestas[listaPreguntas.indexOf(pregunta)])
+        }
+
+        var respuestaUsuario by rememberSaveable {
             mutableStateOf("")
         }
 
@@ -69,37 +92,52 @@ class MainActivity : ComponentActivity() {
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
-        ) {
+        )
+        {
+            Box(modifier = Modifier.padding(16.dp)) {
 
-            OutlinedTextField(value = pregunta,
-                onValueChange = { pregunta = it },
-                label = { Text(text = "Pregunta") })
-
-
-            OutlinedTextField(value = respuesta,
-                onValueChange = { respuesta = it },
-                label = { Text(text = "Respuesta") })
-
-            OutlinedButton(onClick = {
-                if (respuesta == hashMapOf(pregunta to respuesta)[pregunta]) {
-                    puntuacion++
-                    hashMapOf(pregunta to respuesta).remove(pregunta)
-                } else {
-                    Toast.makeText(
-                        context,
-                        "La respuesta correcta era " + hashMapOf(pregunta to respuesta)[pregunta],
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-                pregunta = hashMapOf(
-                    "¿Cuál es el juego más vendido de la historia?" to "Tetris",
-                    "¿Cuál es el mamífero más pesado del mundo?" to "Ballena Azul",
-                    "¿Cuál es el elemento químico con mayor densidad?" to "Osmio",
-                    "¿Cuál es la velocidad del sonido en m/s?" to "344",
-                    "¿Cuál es la capacidad siguiente al Terabyte?" to "Petabyte",
-                ).keys.random()
-                respuesta = ""
             }
+
+            BasicTextField(
+                value = pregunta,
+                onValueChange = { pregunta = it },
+                singleLine = true,
+                modifier = Modifier.padding(16.dp)
+            )
+
+            TextField(
+                value = respuestaUsuario,
+                onValueChange = { respuestaUsuario = it },
+                label = { Text(text = "Respuesta") },
+                modifier = Modifier.padding(16.dp)
+            )
+
+
+            Button(onClick = {
+                if (respuesta == respuestaUsuario) {
+                    listaPreguntas.remove(pregunta)
+                    listaRespuestas.remove(respuesta)
+                    puntuacion++
+                    maxPreguntas++
+                    pregunta = listaPreguntas.random()
+                    respuesta = listaRespuestas[listaPreguntas.indexOf(pregunta)]
+                    Toast.makeText(context, "Respuesta correcta", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(context, "Respuesta incorrecta", Toast.LENGTH_LONG).show()
+                }
+
+                respuestaUsuario = ""
+
+                if (maxPreguntas == 3) {
+                    maxPreguntas = 0
+                    puntuacion = 0
+                    pregunta = listaPreguntas.random()
+                    respuesta = listaRespuestas[listaPreguntas.indexOf(pregunta)]
+                    Toast.makeText(context, "Juego reiniciado", Toast.LENGTH_LONG).show()
+                }
+            }
+
+
             ) {
                 Text(text = "Validar Respuesta")
             }
@@ -108,7 +146,10 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
 }
+
+
 
 
 
